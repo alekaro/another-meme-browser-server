01b92bdfdc18372fef5689646546bcbede818e4a
alekaro <olehsander.karonski@gmail.com>
2020-09-19 23:46:04 +0200

    Deleted no longer needed files and content

--- schema/schema.js
+++ schema/schema.js
@@ -75,38 +75,6 @@ const MemeCommentType = new GraphQLObjectType({
   }),
 });
 
-const BookType = new GraphQLObjectType({
-  name: "Book",
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    genre: { type: GraphQLString },
-    author: {
-      type: AuthorType,
-      resolve(parent, args) {
-        // return _.find(authors, { id: parent.authorId });
-        return Author.findById(parent.authorId);
-      },
-    },
-  }),
-});
-
-const AuthorType = new GraphQLObjectType({
-  name: "Author",
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    age: { type: GraphQLInt },
-    books: {
-      type: new GraphQLList(BookType),
-      resolve(parent, args) {
-        // return _.filter(books, { authorId: parent.id });
-        return Book.find({ authorId: parent.id });
-      },
-    },
-  }),
-});
-
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -164,41 +132,6 @@ const RootQuery = new GraphQLObjectType({
         return MemeComment.find({ memeId: memeId, userId: userId });
       },
     },
-
-    book: {
-      type: BookType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        // return _.find(books, { id: args.id });
-        return Book.findById(args.id);
-      },
-    },
-    author: {
-      type: AuthorType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        // return _.find(authors, { id: args.id });
-        return Author.findById(args.id);
-      },
-    },
-    books: {
-      type: new GraphQLList(BookType),
-      resolve(parent, args) {
-        // return books;
-        return Book.find({});
-      },
-    },
-    authors: {
-      type: new GraphQLList(AuthorType),
-      resolve(parent, args) {
-        // return books;
-        console.log("test1");
-        // return Author.find({});
-        const authorList = Author.find({});
-        console.log(authorList);
-        return authorList;
-      },
-    },
   },
 });
 
@@ -237,38 +170,6 @@ const Mutation = new GraphQLObjectType({
           userId: args.userId,
         });
         return meme.save();
-      },
-    },
-    addAuthor: {
-      type: AuthorType,
-      args: {
-        name: { type: new GraphQLNonNull(GraphQLString) },
-        age: { type: new GraphQLNonNull(GraphQLInt) },
-      },
-      resolve(parent, args) {
-        let author = new Author({
-          name: args.name,
-          age: args.age,
-        });
-        return author.save();
-      },
-    },
-
-    addBook: {
-      type: BookType,
-      args: {
-        name: { type: new GraphQLNonNull(GraphQLString) },
-        genre: { type: new GraphQLNonNull(GraphQLString) },
-        authorId: { type: new GraphQLNonNull(GraphQLID) },
-      },
-      resolve(parent, args) {
-        let book = new Book({
-          name: args.name,
-          genre: args.genre,
-          authorId: args.authorId,
-        });
-
-        return book.save();
       },
     },
   },
